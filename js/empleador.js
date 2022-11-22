222d94b9d0d85a079e80f2249e4f9733d70af4b6
isaac192063 <ichinchillac@ufpso.edu.co>
2022-11-22 14:32:32 -0500

    modificacion del footer y conceptos de js

--- js/empleador.js
+++ js/empleador.js
@@ -1,4 +1,10 @@
 function crearPublicacion(){
-    
-
+    alert("eso carago")
+}
+function asa(){
+    let texto = document.getElementById("eso").value;  //para capturar variables
+    alert(texto)
+    document.getElementById("eso").focus(); //para posiciones el puntero en el elemento seleccionado
+    document.getElementById("eso").value = ""; // para limpiar los campos
+    document.getElementById("a").innerHTML = texto; // para asignar el texto captura y enviarlo al html
 }
