febd9e8009fb66ba3f27fd69e85e4150492e4d0f
SanRN <sromeron@ufpso.edu.co>
2022-11-23 07:24:38 -0500

    animacion de formulario

--- js/empleador.js
+++ js/empleador.js
@@ -8,3 +8,24 @@ function asa(){
     document.getElementById("eso").value = ""; // para limpiar los campos
     document.getElementById("a").innerHTML = texto; // para asignar el texto captura y enviarlo al html
 }
+
+
+
+const a = document.getElementById('nombre').value;
+const b = document.getElementById('apellido').value;
+const c = document.getElementById('doc').value;
+const d = document.getElementById('Ndoc').value;
+const e = document.getElementById('sexo').value;
+const f = document.getElementById('edad').value;
+const g = document.getElementById('fecha').value;
+const h = document.getElementById('dic').value;
+const i = document.getElementById('email').value;
+const j = document.getElementById('cont').value;
+const k = document.getElementById('cont2').value;
+const l = document.getElementById('tipo').value;
+const m = document.getElementById('tel').value;
+console.log(a);
+
+function validacion(){
+    a
+}
